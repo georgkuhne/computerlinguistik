801a7af9590035a1fccfc4f2200c4cd76542ef94
9gkuhne <9gkuhne@149.205.196.131>
2014-06-04 10:47:04 +0200

    asd

--- home.computerlinguistik.persistence/src/home/cpmputerlinguistik/persistence/ConstantsPersistence.java
+++ home.computerlinguistik.persistence/src/home/cpmputerlinguistik/persistence/ConstantsPersistence.java
@@ -2,11 +2,11 @@ package home.cpmputerlinguistik.persistence;
 
 public class ConstantsPersistence {
 	public static final String PREFERENCESTORE_DATABASEPATH = "DATABASEPATH";
-	public static final String PREFERENCESTORE_DATABASENAME = "WBSDATABASE";
-	public static final String PREFERENCESTORE_DATABASENAME_SUFFIX = ".WBS";
+	public static final String PREFERENCESTORE_DATABASENAME = "COMPSDATABASE";
+	public static final String PREFERENCESTORE_DATABASENAME_SUFFIX = ".COMP";
 
-	public static final String PREFERENCESTORE_DATABASEUSERNAME = "WIB";
+	public static final String PREFERENCESTORE_DATABASEUSERNAME = "COMP";
 	public static final String PREFERENCESTORE_DATABASEPW = "WIB";
-	public static final String HBDATASTOREID = "WIBDATASTORE";
+	public static final String HBDATASTOREID = "COMPDATASTORE";
 
 }
