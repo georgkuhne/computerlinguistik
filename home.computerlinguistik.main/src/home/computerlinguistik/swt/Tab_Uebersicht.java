a9ae43b697ff86a49c7f2cdc4c11fc061fb779d1
Justus <Justus@149.205.235.228>
2014-06-18 10:28:56 +0200

    Erneuerung der Übersicht

--- home.computerlinguistik.main/src/home/computerlinguistik/swt/Tab_Uebersicht.java
+++ home.computerlinguistik.main/src/home/computerlinguistik/swt/Tab_Uebersicht.java
@@ -1,10 +1,13 @@
 package home.computerlinguistik.swt;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Table;
+import home.cpmputerlinguistik.persistence.PersistenceUtility;
+
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.hibernate.Session;
 
 public class Tab_Uebersicht extends Composite {
 	private Table table;
@@ -18,21 +21,24 @@ public class Tab_Uebersicht extends Composite {
 		super(parent, style);
 		
 		Button btnNeuesWortAnlegen = new Button(this, SWT.NONE);
-		btnNeuesWortAnlegen.setBounds(10, 10, 117, 23);
-		btnNeuesWortAnlegen.setText("Neues Wort anlegen");
+		btnNeuesWortAnlegen.setBounds(10, 10, 145, 25);
+		btnNeuesWortAnlegen.setText("Neues Wort  anlegen");
 		
 		TableViewer tableViewer = new TableViewer(this, SWT.BORDER | SWT.FULL_SELECTION);
 		table = tableViewer.getTable();
-		table.setBounds(10, 39, 743, 420);
+		table.setBounds(10, 41, 745, 416);
 		
-		Button btnEintragBearbeiten = new Button(this, SWT.NONE);
-		btnEintragBearbeiten.setBounds(10, 465, 101, 23);
-		btnEintragBearbeiten.setText("Eintrag bearbeiten");
+		Button button = new Button(this, SWT.NONE);
+		button.setBounds(10, 463, 75, 25);
+		button.setText("1");
 		
-		Button btnEintragLschen = new Button(this, SWT.NONE);
-		btnEintragLschen.setBounds(117, 465, 101, 23);
-		btnEintragLschen.setText("Eintrag l\u00F6schen");
+		Button button_1 = new Button(this, SWT.NONE);
+		button_1.setBounds(91, 463, 75, 25);
+		button_1.setText("2");
 
+		
+		Session session = PersistenceUtility.getINSTANCE().createSession();
+		
 	}
 
 	@Override
